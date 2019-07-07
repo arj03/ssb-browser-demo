bc33fd8115658f5a5f6a29e4e0100d2ee7c0cab4
Anders Rune Jensen <arj03@protonmail.ch>
2019-07-07 22:16:01 +0200

    Onion connections

--- test.js
+++ test.js
@@ -15,22 +15,29 @@ var db = DB.init(dir)
 
 var app = SecretStack({
   caps: { shs: Buffer.from(caps.shs, 'base64') },
-  keys
+  keys,
+  connections: {
+    outgoing: {
+      net: [{ transform: 'shs' }],
+      onion: [{ transform: 'shs' }]
+    }
+  }
 })
 .use(require('./hist'))
 .use(require('./simple-ooo'))
-//    .use(require('ssb-onion'))
+.use(require('ssb-onion'))
 ()
 
 var msgId = "%IwG4GtadWmHUhsn+YJZBXs9D7/wnPtlTuVOTVrPl+0o=.sha256"
 var feedId = "@6CAxOI3f+LUOVrbAl0IemqiS7ATpQvr9Mdw9LC4+Uv0=.ed25519"
-//var remoteAddress = "onion:4fqstkswahy3n7mupr2gvvp2qcsp6juwzn3mnqvhkaixxepvxrrtfbid.onion:8008~shs:lbocEWqF2Fg6WMYLgmfYvqJlMfL7hiqVAV6ANjHWNw8="
-var remoteAddress = "net:ssb.celehner.com:8008~shs:5XaVcAJ5DklwuuIkjGz4lwm2rOnMHHovhNg7BFFnyJ8="
+var remoteAddress = "onion:4fqstkswahy3n7mupr2gvvp2qcsp6juwzn3mnqvhkaixxepvxrrtfbid.onion:8008~shs:lbocEWqF2Fg6WMYLgmfYvqJlMfL7hiqVAV6ANjHWNw8="
+//var remoteAddress = "net:ssb.celehner.com:8008~shs:5XaVcAJ5DklwuuIkjGz4lwm2rOnMHHovhNg7BFFnyJ8="
 
 app.connect(remoteAddress, (err, rpc) => {
+  if (err) throw(err)
 
   //console.log(rpc)
-  
+
   /*
   app.ooo.get(msgId, (err, msg) => {
     console.log("err", err)
