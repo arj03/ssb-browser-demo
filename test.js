9b99797f3218b007b6a1f89513be0d0850215d48
Anders Rune Jensen <arj03@protonmail.ch>
2019-07-08 22:41:01 +0200

    blobs

--- test.js
+++ test.js
@@ -21,22 +21,34 @@ var app = SecretStack({
       net: [{ transform: 'shs' }],
       onion: [{ transform: 'shs' }]
     }
-  }
+  },
+  path: dir
 })
 .use(require('./hist'))
 .use(require('./simple-ooo'))
 .use(require('ssb-onion'))
+.use(require('ssb-blobs'))
 ()
 
 var msgId = "%IwG4GtadWmHUhsn+YJZBXs9D7/wnPtlTuVOTVrPl+0o=.sha256"
 var feedId = "@6CAxOI3f+LUOVrbAl0IemqiS7ATpQvr9Mdw9LC4+Uv0=.ed25519"
-var remoteAddress = "onion:4fqstkswahy3n7mupr2gvvp2qcsp6juwzn3mnqvhkaixxepvxrrtfbid.onion:8008~shs:lbocEWqF2Fg6WMYLgmfYvqJlMfL7hiqVAV6ANjHWNw8="
-//var remoteAddress = "net:ssb.celehner.com:8008~shs:5XaVcAJ5DklwuuIkjGz4lwm2rOnMHHovhNg7BFFnyJ8="
+var blobId = "&Il2SFDKScJcqt3CTl+ZaeIJLXGwmPbQHUTi9lVaUH5c=.sha256"
+
+//var remoteAddress = "onion:4fqstkswahy3n7mupr2gvvp2qcsp6juwzn3mnqvhkaixxepvxrrtfbid.onion:8008~shs:lbocEWqF2Fg6WMYLgmfYvqJlMfL7hiqVAV6ANjHWNw8="
+var remoteAddress = "net:ssb.celehner.com:8008~shs:5XaVcAJ5DklwuuIkjGz4lwm2rOnMHHovhNg7BFFnyJ8="
+remoteAddress = "net:eight45.net:8008~shs:eM4e8pmRiZpeCBitqp6vq3lT8EwC5UjjKuajHbpWnNI="
 
 app.connect(remoteAddress, (err, rpc) => {
   if (err) throw(err)
 
   //console.log(rpc)
+
+  /*
+  pull(
+    rpc.blobs.get({key: blobId}),
+    app.blobs.add(blobId) // save locally
+  )
+  */
 
   /*
   app.ooo.get(msgId, (err, msg) => {
