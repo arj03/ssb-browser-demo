77d9edc05d5b3275bf49352b742ed08b2c83e837
Anders Rune Jensen <arj03@protonmail.ch>
2020-10-16 22:13:30 +0200

    Enable connecting to staged peers

--- ui/connections.js
+++ ui/connections.js
@@ -1,3 +1,5 @@
+const pull = require('pull-stream')
+
 module.exports = function () {
   return {
     template: `
@@ -13,16 +15,22 @@ module.exports = function () {
         <input type="text" placeholder="remote address" v-model="address" id="remoteAddress" />
         <button class="clickButton" v-on:click="add">Add</button>
       </div>
+      <h3>Staged peers</h3>
+      <div v-for="stagedPeer in stagedPeers">
+        <button class="clickButton" v-on:click="connect(stagedPeer)">Connect to {{ stagedPeer.data.key }}</button>
+      </div>
       <div id="status" v-html="statusHTML"></div>
     </div>`,
 
     data: function() {
       return {
         type: 'pub',
+        // room: wss:between-two-worlds.dk:9999~shs:7R5/crt8/icLJNpGwP2D7Oqz2WUd7ObCIinFKVR6kNY=
         address: 'wss:between-two-worlds.dk:8989~shs:lbocEWqF2Fg6WMYLgmfYvqJlMfL7hiqVAV6ANjHWNw8=',
 
         statusHTML: '',
         running: true,
+        stagedPeers: []
       }
     },
 
@@ -33,6 +41,9 @@ module.exports = function () {
           key: '@' + s[s.length-1] + '.ed25519',
           type: this.type
         })
+      },
+      connect: function(stagedPeer) {
+        SSB.net.connectAndRemember(stagedPeer.address, stagedPeer.data)
       }
     },
 
@@ -41,6 +52,13 @@ module.exports = function () {
 
       var lastStatus = null
       var lastEbtStatus = null
+
+      pull(
+        SSB.net.conn.stagedPeers(),
+        pull.drain((entries) => {
+          self.stagedPeers = entries.filter(([, x]) => !!x.key).map(([address, data]) => ({ address, data }))
+        })
+      )
 
       function updateDBStatus() {
         if (!self.running) return
