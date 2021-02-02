9641a9581aade11f369706388ebc2e71a32a6297
Kyle Maas <kylemaasdev@gmail.com>
2021-02-02 08:28:50 -0500

    Optimize theme images

--- write-dist.js
+++ write-dist.js
@@ -40,7 +40,7 @@ rimraf("dist", function () {
   fs.copyFileSync('assets/noavatar.svg', 'dist/assets/noavatar.svg')
   fs.copyFileSync('assets/openclipart-1392738806.svg', 'dist/assets/openclipart-1392738806.svg')
   fs.copyFileSync('assets/openclipart-261618.svg', 'dist/assets/openclipart-261618.svg')
-  fs.copyFileSync('assets/openclipart-223534.svg', 'dist/assets/openclipart-223534.svg')
+  fs.copyFileSync('assets/openclipart-223534.png', 'dist/assets/openclipart-223534.png')
   fs.copyFileSync('assets/seabed-155896303267m-optimized.jpg', 'dist/assets/seabed-155896303267m-optimized.jpg')
   fs.copyFileSync('assets/old-barn-wood-background-recolored-2-tiled.jpg', 'dist/assets/old-barn-wood-background-recolored-2-tiled.jpg')
 
