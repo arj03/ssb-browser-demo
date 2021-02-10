c54c403ea5f92fbfd57da791bf963d2132b8fba6
Kyle Maas <kylemaasdev@gmail.com>
2021-02-10 10:30:50 -0500

    Use FlexSearch instead of MiniSearch

--- search.js
+++ search.js
@@ -1,4 +1,3 @@
-const MiniSearch = require('minisearch').default
 const { and, descending, paginate, type, toPullStream } = SSB.dbOperators
 const pull = require('pull-stream')
 const localPrefs = require('./localprefs')
@@ -18,7 +17,7 @@ function indexNewPosts(cb) {
         if (msgs[m].key == mostRecentMessage)
           break
         if (msgs[m].value && msgs[m].value.content && msgs[m].value.content.text)
-          SSB.search.miniSearch.add({ key: msgs[m].key, text: msgs[m].value.content.text })
+          SSB.search.flexSearchIndex.add({ id: msgs[m].key, key: msgs[m].key, text: msgs[m].value.content.text })
       }
       if (msgs && msgs.length > 0)
         mostRecentMessage = msgs[0].key
@@ -30,9 +29,11 @@ function indexNewPosts(cb) {
 if (!SSB.search) {
   SSB.search = {
     depth: localPrefs.getSearchDepth(),
-    miniSearch: new MiniSearch({
-      fields: ['text'],
-      idField: 'key'
+    flexSearchIndex: FlexSearch.create("speed", {
+      doc: {
+        id: "key",
+        field: "text"
+      }
     }),
     resetIndex: function() {
       mostRecentMessage = null
@@ -40,23 +41,13 @@ if (!SSB.search) {
     fullTextSearch: function(searchTerm, cb) {
       indexNewPosts(() => {
         try {
-          var results = SSB.search.miniSearch.search(searchTerm, { fuzzy: 0.1 })
+          SSB.search.flexSearchIndex.search(searchTerm, (results) => {
+            cb(null, results)
+          })
         } catch(e) {
           cb(e)
           return
         }
-
-        // Sometimes MiniSearch returns duplicates.  Deduplicate them.
-        var seenKeys = []
-        var filteredResults = []
-        for (r in results) {
-          if (seenKeys.indexOf(results[r].id) < 0) {
-            filteredResults.push(results[r])
-            seenKeys.push(results[r].id)
-          }
-        }
-
-        cb(null, filteredResults)
       })
     }
   }
