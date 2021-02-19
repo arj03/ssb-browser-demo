dd6e7abdd16662561e5964d5f3786991d66bc52d
Kyle Maas <kylemaasdev@gmail.com>
2021-02-19 14:59:42 -0500

    Fix this.created error

--- ui/ssb-profile-name-link.js
+++ ui/ssb-profile-name-link.js
@@ -19,35 +19,39 @@ Vue.component('ssb-profile-name-link', {
       const self = this
       if (self.feedId != SSB.net.id)
         self.name = profile.name
+    },
+
+    refresh: function () {
+      const self = this
+  
+      if (self.feedId == SSB.net.id)
+        self.name = this.$root.$t('common.selfPronoun')
+  
+      SSB.net.friends.isBlocking({ source: SSB.net.id, dest: self.feedId }, (err, result) => {
+        if (!err) self.isBlocked = result
+      })
+  
+      const profile = SSB.getProfile(self.feedId)
+      if (profile.name || profile.imageURL || profile.image) {
+        self.renderProfile(profile)
+      } else {
+        // Try one more time after the profile index has loaded.
+        setTimeout(() => {
+          const profileAgain = SSB.getProfile(self.feedId)
+          if (profileAgain)
+            self.renderProfile(profileAgain)
+        }, 3000)
+      }
     }
   },
 
-  created: function () {
-    const self = this
-
-    if (self.feedId == SSB.net.id)
-      self.name = this.$root.$t('common.selfPronoun')
-
-    SSB.net.friends.isBlocking({ source: SSB.net.id, dest: self.feedId }, (err, result) => {
-      if (!err) self.isBlocked = result
-    })
-
-    const profile = SSB.getProfile(self.feedId)
-    if (profile.name || profile.imageURL || profile.image) {
-      self.renderProfile(profile)
-    } else {
-      // Try one more time after the profile index has loaded.
-      setTimeout(() => {
-        const profileAgain = SSB.getProfile(self.feedId)
-        if (profileAgain)
-          self.renderProfile(profileAgain)
-      }, 3000)
-    }
+  created: function() {
+    this.refresh()
   },
 
   watch: {
     feedId: function (oldValue, newValue) {
-      this.created()
+      this.refresh()
     }
   }
 })
